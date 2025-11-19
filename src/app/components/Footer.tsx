45d9b6d9d07faf9faa10088c6d19480e88a6d0df
Keny2802 <oliva.vojta@gmail.com>
2025-11-19 15:18:55 +0100

    Přidání Footer komponenty.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,15 +1,17 @@
 import {
-    useState,
     Fragment,
     ReactNode
 } from "react";
 
 import Wrapper from "./Wrapper";
+import Logo from "./Logo";
 
 const Footer = () => {
     return (
         <Fragment>
-            <Wrapper></Wrapper>
+            <Wrapper className="px-4 md:px-24 py-4 md:py-16 text-gray-300 bg-linear-to-tr from-[#050b1a] via-[#0b1f3b] to-[#12345a] border-t border-gray-500 footer-wrapper">
+                <Logo />
+            </Wrapper>
         </Fragment>
     );
 };
